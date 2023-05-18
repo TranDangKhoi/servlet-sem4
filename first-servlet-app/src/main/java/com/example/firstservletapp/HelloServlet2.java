83069f2cc4057c9d7bd2e3f2f8f2bcf9c45ca05f
TranDangKhoi <baits16@gmail.com>
2023-05-18 22:38:47 +0700

    Config Java Servlet using Annotations

---
+++ first-servlet-app/src/main/java/com/example/firstservletapp/HelloServlet2.java
@@ -0,0 +1,20 @@
+package com.example.firstservletapp;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = {"/xin-chao2", "/hello2"})
+public class HelloServlet2 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.println("Xin chao moi nguoi!");
+        printWriter.close();
+    }
+}
